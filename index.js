8709ab22e48a3a885042ee3c58ea4f5a5ae65595
alaminarmanjanina <81838250+alaminarmanjanina@users.noreply.github.com>
2022-11-05 01:17:14 +0600

    password secured by .env file

---
+++ index.js
@@ -0,0 +1,24 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vycotd7.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+app.get("/", (req, res) => {
+  res.send("car server running");
+});
+
+app.listen(port, () => {
+  console.log(`genius car server running on port ${port}`);
+});
