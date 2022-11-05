79392d669e8d87e4e578573862e6b40f64394049
alaminarmanjanina <81838250+alaminarmanjanina@users.noreply.github.com>
2022-11-05 22:55:16 +0600

    jwt token works perfectly

--- index.js
+++ index.js
@@ -24,7 +24,10 @@ async function run() {
 
     app.post("jwt", (req, res) => {
       const user = req.body;
-      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expireIn: "1h",
+      });
+      res.send({ token });
     });
 
     app.get("/services", async (req, res) => {
